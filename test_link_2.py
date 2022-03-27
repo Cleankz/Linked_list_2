c892a15a8714058588a5144300b24bd90f88bbf7
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-03-27 18:41:17 +0300

    Create test_link_2.py

---
+++ test_link_2.py
@@ -0,0 +1,12 @@
+import unittest
+from Link_2 import Node
+from Link_2 import LinkedList2
+
+class MyTests(unittest.TestCase):
+    def test_add_in_tail():
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
